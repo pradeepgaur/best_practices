b8aa6543e3fd1ea781c7f03e0e0568cbaf76b7ac
pradeepgaur <pradeepcitm@gmail.com>
2021-08-06 18:31:00 +0530

    This is test

---
+++ unit tests/load_clean_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+def load_clean_data(file_path):
+    df = pd.read_csv(file_path) 
+    return df
+
+# writing tests to ensure loaded data is in expected format.
+
+FILE_PATH = "/content/sample_data/california_housing_train.csv"
+
+def test_missing_values_load_clean_data_california():
+    df = load_clean_data(FILE_PATH)
+    
+    #longitude and latitude should not have missing values
+    assert df['longitude'].isna().sum() < 1
+    assert df['latitude'].isna().sum() < 1
+
+def test_data_shape_load_clean_data_california():
+    df = load_clean_data(FILE_PATH)
+    
+    # testing for any missing or additional columns
+    assert df.shape[1] == 9
+
+    # checking if label present 
+    assert 'median_house_value' in df.columns
+    
+
+
+
