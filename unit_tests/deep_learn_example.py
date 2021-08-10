0968799c7c1d6128343fd0f0e6a962c95e1c5dbe
pradeepgaur <pradeepcitm@gmail.com>
2021-08-10 15:16:06 +0530

    Made a change to fail a test

---
+++ unit_tests/deep_learn_example.py
@@ -0,0 +1,26 @@
+
+def make_convnet(input_image):
+    net = slim.conv2d(input_image, 32, [11, 11], scope="conv1_11x11")
+    net = slim.conv2d(input_image, 64, [5, 5], scope="conv2_5x5")
+    net = slim.max_pool2d(net, [4, 4], stride=4, scope='pool1')
+    net = slim.conv2d(input_image, 64, [5, 5], scope="conv3_5x5")
+    net = slim.conv2d(input_image, 128, [3, 3], scope="conv4_3x3")
+    net = slim.max_pool2d(net, [2, 2], scope='pool2')
+    net = slim.conv2d(input_image, 128, [3, 3], scope="conv5_3x3")
+    net = slim.max_pool2d(net, [2, 2], scope='pool3')
+    net = slim.conv2d(input_image, 32, [1, 1], scope="conv6_1x1")
+    return net
+
+def test_convnet():
+  image = tf.placeholder(tf.float32, (None, 100, 100, 3)
+  model = Model(image)
+  sess = tf.Session()
+  sess.run(tf.global_variables_initializer())
+  before = sess.run(tf.trainable_variables())
+  _ = sess.run(model.train, feed_dict={
+                    image: np.ones((1, 100, 100, 3)),
+                })
+  after = sess.run(tf.trainable_variables())
+  for b, a, n in zip(before, after):
+      # Make sure something changed.
+      assert (b != a).any()
